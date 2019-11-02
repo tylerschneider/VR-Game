bdb40e3dcfd22988fb6e8787e962c88a81283d60
tylerschneider <tylerschneider@u.boisestate.edu>
2019-11-02 00:08:16 -0600

    cut

---
+++ Assets/Cuttable.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EzySlice;
+
+public class Cuttable : MonoBehaviour
+{
+    public Vector3 startPoint;
+    public Vector3 endPoint;
+    private GameObject cutPlane;
+    public string cutting;
+
+    public Joint topJoint;
+    public Joint bottomJoint;
+
+    public float cancelCutSeconds;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Sword")
+        {
+            if(cutting == "")
+            {
+                cutting = "started";
+
+                startPoint = collision.GetContact(0).point;
+
+                StartCoroutine(stopCut());
+            }
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Sword")
+        {
+            Debug.Log(collision.gameObject);
+            if (cutting == "started")
+            {
+                if (GetComponent<Collider>().bounds.Contains(collision.transform.Find("BladeTip").transform.position))
+                {
+                    cutting = "canceled";
+                    Debug.Log("didn't fully cut");
+                    StopCoroutine(stopCut());
+                }
+
+                endPoint = collision.GetContact(collision.contactCount - 1).point;
+            }
+
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Sword")
+        {
+            if(cutting == "started")
+            {
+                cutPlane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+                cutPlane.transform.position = startPoint;
+                Vector3 rotLocation = endPoint - startPoint;
+                Quaternion rotation = Quaternion.LookRotation(rotLocation);
+                cutPlane.transform.rotation = rotation;
+
+
+                SlicedHull newSlice = SliceObject(gameObject, null);
+
+                if (newSlice != null)
+                {
+                    GameObject topSlice = newSlice.CreateLowerHull(gameObject, gameObject.GetComponent<Renderer>().material);
+                    GameObject bottomSlice = newSlice.CreateUpperHull(gameObject, gameObject.GetComponent<Renderer>().material);
+                    Destroy(cutPlane);
+
+                    Rigidbody trb = topSlice.AddComponent<Rigidbody>();
+                    topSlice.AddComponent<MeshCollider>().convex = true;
+                    if(topJoint != null)
+                    {
+                        topSlice.AddComponent<Joint>().connectedBody = topJoint.connectedBody;
+                    }
+                    Rigidbody brb = bottomSlice.AddComponent<Rigidbody>();
+                    bottomSlice.AddComponent<MeshCollider>().convex = true;
+                    if (bottomJoint != null)
+                    {
+                        bottomSlice.AddComponent<Joint>().connectedBody = bottomJoint.connectedBody;
+                    }
+
+                    trb.AddExplosionForce(50, endPoint + (startPoint + endPoint) / 2, 10);
+                    brb.AddExplosionForce(50, endPoint + (startPoint + endPoint) / 2, 10);
+                    //trb.AddForce(collision.relativeVelocity);
+
+                    topSlice.AddComponent<Cuttable>();
+                    bottomSlice.AddComponent<Cuttable>();
+
+                    Destroy(gameObject);
+                }
+            }
+            else
+            {
+                cutting = "";
+            }
+        }
+    }
+
+    private IEnumerator stopCut()
+    {
+        yield return new WaitForSeconds(cancelCutSeconds);
+        cutting = "canceled";
+        Debug.Log("Timeout");
+    }
+
+    public SlicedHull SliceObject(GameObject obj, Material crossSectionMaterial)
+    {
+        // slice the provided object using the transforms of this object
+        return obj.Slice(cutPlane.transform.position, cutPlane.transform.up, crossSectionMaterial);
+    }
+
+}
--- Assets/Scripts/swordScript.cs
+++ Assets/Scripts/swordScript.cs
@@ -1,93 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using EzySlice;
 
 public class swordScript : MonoBehaviour
 {
     public Transform swordSlot;
-    public GameObject objectToSlice;
-    public Vector3 startPoint;
-    public Vector3 endPoint;
-    public GameObject cutPlane;
-
-    public GameObject col;
-    public float cancelCutSeconds;
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log(collision.gameObject);
-        if (collision.gameObject.tag == "Cuttable")
-        {
-            if(col == null)
-            {
-                Debug.Log(col);
-                col = collision.gameObject;
-                startPoint = collision.GetContact(0).point;
-                Debug.Log(col.gameObject);
-                objectToSlice = collision.gameObject.transform.parent.gameObject;
-                Debug.Log(objectToSlice);
-
-                StartCoroutine(stopCut());
-            }
-            else if(collision.gameObject != col)
-            {
-                endPoint = collision.GetContact(collision.contactCount - 1).point;
-            }
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.tag == "Cuttable")
-        {
-            if (collision.gameObject != col && collision.gameObject.transform.parent.gameObject == objectToSlice)
-            {
-                    Debug.Log(startPoint);
-                    Debug.Log(endPoint);
-
-                    cutPlane = GameObject.CreatePrimitive(PrimitiveType.Plane);
-                    cutPlane.transform.position = startPoint;
-                    Vector3 rotLocation = endPoint - startPoint;
-                    Quaternion rotation = Quaternion.LookRotation(rotLocation);
-                    cutPlane.transform.rotation = rotation;
-
-
-                SlicedHull newSlice = SliceObject(objectToSlice, null);
-
-                    if (newSlice != null)
-                    {
-                        GameObject topSlice = newSlice.CreateLowerHull(objectToSlice, null);
-                        GameObject bottomSlice = newSlice.CreateUpperHull(objectToSlice, null);
-                        Destroy(objectToSlice);
-                        Destroy(cutPlane);
-
-                        Rigidbody trb = topSlice.AddComponent<Rigidbody>();
-                        topSlice.AddComponent<MeshCollider>().convex = true;
-                        Rigidbody brb = bottomSlice.AddComponent<Rigidbody>();
-                        bottomSlice.AddComponent<MeshCollider>().convex = true;
-
-                    trb.AddExplosionForce(50, endPoint + (startPoint + endPoint) / 2, 10);
-                    brb.AddExplosionForce(50, endPoint + (startPoint + endPoint) / 2, 10);
-
-                    col = null;
-                    }
-
-            }
-        }
-    }
-
-    private IEnumerator stopCut()
-    {
-        yield return new WaitForSeconds(cancelCutSeconds);
-        col = null;
-    }
-
-    public SlicedHull SliceObject(GameObject obj, Material crossSectionMaterial = null)
-    {
-        // slice the provided object using the transforms of this object
-        return obj.Slice(cutPlane.transform.position, cutPlane.transform.up, crossSectionMaterial);
-    }
 
     void Update()
     {
