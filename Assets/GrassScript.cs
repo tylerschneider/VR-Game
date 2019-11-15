a3e8b95c8c7c17dbc17d95f8b3225c313e842ddb
tylerschneider <46699128+tylerschneider@users.noreply.github.com>
2019-11-15 16:13:51 -0700

    Grass and shaders

--- Assets/GrassScript.cs
+++ Assets/GrassScript.cs
@@ -11,26 +11,32 @@ public class GrassScript : MonoBehaviour
 
     void Update()
     {
-        float playerDist = Vector3.Distance(Player.Instance.transform.position, transform.position);
-        if (Vector3.Distance(Player.Instance.transform.position, transform.position) < minDist)
+        Vector3 playerPos = Player.Instance.transform.position;
+        Vector3 playerDist = new Vector3(playerPos.x - transform.position.x, 0, playerPos.z - transform.position.z);
+        if (playerDist.magnitude < minDist)
         {
 
-            //transform.LookAt(Player.Instance.transform);
-            Quaternion rot = transform.rotation;
+            transform.LookAt(playerPos);
+            Quaternion rot2 = transform.rotation;
+            rotateAmount = ((minDist - playerDist.magnitude) / (minDist - maxDist)) * maxRot;
+            transform.eulerAngles = new Vector3(-rotateAmount, rot2.eulerAngles.y, rot2.eulerAngles.z);
 
-            rotateAmount = ((minDist - playerDist) / (minDist - maxDist)) * maxRot;
+            Transform grass = transform.Find("Model");
+            grass.transform.rotation = Quaternion.LookRotation(Vector3.forward);
+            Vector3 rot = grass.transform.localEulerAngles;
+            grass.transform.localEulerAngles = Vector3.Scale(rot, new Vector3(0, 1, 0));
 
-            //transform.rotation = Quaternion.Euler(rot.x, rot.y, rotateAmount);
 
-            float angle = Mathf.Atan2(transform.position.x - Player.Instance.transform.position.x, transform.position.z - Player.Instance.transform.position.z) * Mathf.Rad2Deg;
+            /*float angle = Mathf.Atan2(transform.position.x - Player.Instance.transform.position.x, transform.position.z - Player.Instance.transform.position.z) * Mathf.Rad2Deg;
             //Debug.Log(angle + " Sin: " + Mathf.Sin(angle) * minDist + " Cos: " + Mathf.Cos(angle) * minDist + " Tan: " + Mathf.Tan(angle) * minDist);
 
             transform.rotation = Quaternion.Euler(-angle, 0, -angle);
-            Debug.Log(transform.rotation.x + " " + transform.rotation.z);
+            Debug.Log(transform.rotation.x + " " + transform.rotation.z);*/
         }
         else
         {
             transform.rotation = Quaternion.Euler(Vector3.zero);
+            transform.Find("Model").transform.rotation = Quaternion.Euler(Vector3.zero);
         }
     }
 }
